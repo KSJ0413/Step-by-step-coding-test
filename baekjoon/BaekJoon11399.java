e39583f2aa6831408968aa7a2f0290906344beab
KSJ0413 <rlatjdwls2525@gmail.com>
2022-08-21 14:46:45 +0900

    beekjoon

--- baekjoon/BaekJoon11399.java
+++ baekjoon/BaekJoon11399.java
@@ -7,21 +7,107 @@ public class BaekJoon11399 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int N = scanner.nextInt();
-        int total=0;
-        int[] P = new int[N];
-        for(int i=0; i<N; i++) {
-            P[i] = scanner.nextInt();
-        }
-        Arrays.sort(P); //정렬
+        int K = scanner.nextInt();
+        int U =0;
 
-        int pre = 0;	//이전까지의 대기시간 합
-        int sum = 0;	//사람 별 대기시간 총합
+        int one =   scanner.nextInt();
+        int five = scanner.nextInt();
+        int ten = scanner.nextInt();
+        int five_ten = scanner.nextInt();
+        int tenten = scanner.nextInt();
+        int five_tenten = scanner.nextInt();
+        int tententen = scanner.nextInt();
+        int five_tententen = scanner.nextInt();
+        int tententententen = scanner.nextInt();
+        int five_tentententen = scanner.nextInt();
 
-        for(int i = 0; i < N; i++) {
-            sum += pre + P[i];
-            pre += P[i];
+
+
+        if(K>five_tentententen) {
+            U =+ K/five_tentententen;
+            int K1 = K%five_tentententen;
+            if(K1>tententententen) {
+                U =+ K/tententententen;
+                int K2 = K1%tententententen;
+                if(K2>five_tententen) {
+                    U =+ K/five_tententen;
+                    int K3=K%five_tententen;
+                    if(K3>tententen) {
+                        U =+ K/tententen;
+                        int K4=K%tententen;
+                        if(K4>five_tenten) {
+                            U =+ K/five_tenten;
+                            int K5=K%five_tenten;
+                            if(K5>tenten) {
+                                U =+ K/tenten;
+                                int K6=K%tenten;
+                                if(K6>five_ten) {
+                                    U =+ K/five_ten;
+                                    int K7=K%five_ten;
+                                    if(K7>ten) {
+                                        U =+ K/ten;
+                                        int K8=K%ten;
+                                        if(K8>five) {
+                                            U =+ K/five;
+                                            int K9=K%five;
+                                            if(K9>one) {
+                                                U =+ K/one;
+                                                int K10=K%one;
+                                                System.out.println(U);
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
         }
-        System.out.println(sum);
+
+        }
+
+
+        if(K1>tententententen) {
+            U =+ K/tententententen;
+            int K2 = K1%tententententen;
+            if(K2>five_tententen) {
+                U =+ K/five_tententen;
+                int K3=K%five_tententen;
+                if(K3>tententen) {
+                    U =+ K/tententen;
+                    int K4=K%tententen;
+                    if(K4>five_tenten) {
+                        U =+ K/five_tenten;
+                        int K5=K%five_tenten;
+                        if(K5>tenten) {
+                            U =+ K/tenten;
+                            int K6=K%tenten;
+                            if(K6>five_ten) {
+                                U =+ K/five_ten;
+                                int K7=K%five_ten;
+                                if(K7>ten) {
+                                    U =+ K/ten;
+                                    int K8=K%ten;
+                                    if(K8>five) {
+                                        U =+ K/five;
+                                        int K9=K%five;
+                                        if(K9>one) {
+                                            U =+ K/one;
+                                            int K10=K%one;
+                                            System.out.println(U);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+
+
+        }
+
 
        }
-    }
