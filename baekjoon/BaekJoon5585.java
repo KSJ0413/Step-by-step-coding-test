6208fdb8b024924cdc8bc89c610f8bcfac3e3223
KSJ0413 <rlatjdwls2525@gmail.com>
2022-08-31 07:35:14 +0900

    다른 풀이 설명

--- baekjoon/BaekJoon5585.java
+++ baekjoon/BaekJoon5585.java
@@ -23,3 +23,21 @@ public class BaekJoon5585 {
         System.out.println(result);
     }
 }
+//    지불해야 할 금액을 입력받고 solve함수를 호출합니다. **
+//    public static void main(String[] args) throws IOException {
+//        int pay = Integer.parseInt(br.readLine());
+//
+//        System.out.println(solve(1000 - pay));
+//    }
+//
+//     최소 거스름돈 개수를 구하는 함수입니다. ** 
+//    private static int solve(int changeMoney) {
+//        int[] change = {500, 100, 50, 10, 5, 1};
+//        int result = 0;
+//
+//        for (int index = 0; index < change.length && changeMoney >= 0; index++) {
+//            result += changeMoney / change[index];
+//            changeMoney %= change[index];
+//        }
+//        return result;
+//    }
